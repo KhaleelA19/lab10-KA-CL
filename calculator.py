57ab6a8ffa21d6536782461cb7ca4a309e77ef15
KhaleelA19 <khaleelabiru@ufl.edu>
2025-04-12 16:07:31 -0400

    Merged Changes

--- calculator.py
+++ calculator.py
@@ -1,5 +1,22 @@
 import math
 
+def add(a,b):
+    return a+b
+def subtract(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    if a == 0:
+        raise ZeroDivisionError("Cannot divide by zero")
+    else:
+        return b/a
+def logarithm(a,b):
+    if b <= 0 or a <= 1:
+            raise ValueError("Base must be greater than 1 and Argument must be greater than 0")
+    return math.log(b,a)
+def exp(a,b):
+    return a**b
 
 def square_root(a):
     if a >= 0:
@@ -9,20 +26,3 @@ def square_root(a):
 
 def hypotenuse(a, b):
     return math.hypot(a, b)
-def add(a,b):
-    return a+b
-def sub(a,b):
-    return a-b
-def mul(a,b):
-    return a*b
-def div(a,b):
-    if a == 0:
-        raise ZeroDivisionError("Cannot divide by zero")
-    else:
-        return b/a
-def log(a,b):
-    if b <= 0 or a <= 1:
-            raise ValueError("Base must be greater than 1 and Argument must be greater than 0")
-    return math.log(b,a)
-def exp(a,b):
-    return a**b
