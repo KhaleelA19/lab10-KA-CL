15b7f225dca0de81dbf06d4dd34d1da05ec205b6
camilalipson <camilalipson@ufl.edu>
2025-04-12 15:44:01 -0400

    part 1 done

--- calculator.py
+++ calculator.py
@@ -1,27 +1,19 @@
 import math
 
-def square_root(a):
-    if a >= 0:
-        return math.sqrt(a)
-    else:
-        raise ValueError("Cannot calculate square root of negative numbers.")
-def hypotenuse(a,b):
-    return math.hypot(a,b)
 def add(a,b):
     return a+b
-def subtract(a,b):
+def sub(a,b):
     return a-b
-def multiply(a,b):
+def mul(a,b):
     return a*b
-def divide(a,b):
+def div(a,b):
     if a == 0:
         raise ZeroDivisionError("Cannot divide by zero")
     else:
         return b/a
-def logarithm(a,b):
+def log(a,b):
     if b <= 0 or a <= 1:
             raise ValueError("Base must be greater than 1 and Argument must be greater than 0")
     return math.log(b,a)
-def exponent(a,b):
+def exp(a,b):
     return a**b
-    print("Camnila is my partner")
